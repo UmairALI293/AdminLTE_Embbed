375836329ec14a8fd8a7917d41a6373c194130b5
UMAIR ALi <umairali29300293@gmail.com>
2024-06-05 18:02:19 +0500

    Admin_LTE Theme

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TekAdminLTE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
